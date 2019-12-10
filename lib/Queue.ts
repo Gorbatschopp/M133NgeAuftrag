5a39ceaa23a6fe181016ebab4093524ef8daad88
Nikola Gerun <n.gerun@novacapta.com>
2019-12-10 14:03:45 +0100

    Added requested queue functionality and tests

--- lib/Queue.ts
+++ lib/Queue.ts
@@ -1,7 +1,33 @@
 import { IDataScructure } from "./IDataStructure";
 
-export class Queue implements IDataScructure {
-    public size(): number {
-        return undefined;
+export class Queue<T> implements IDataScructure 
+{
+    _storage: T[] = [];
+
+    public size(): number 
+    {
+        return this._storage.length;
+    }
+
+    public enqueue(value: T)
+    {
+        this._storage.push(value);
+    }
+
+    public peek()
+    {
+        var firstElement = this._storage.shift();
+        this.enqueue(firstElement);
+        return firstElement;
+    }
+
+    public poll()
+    {
+        return this._storage.shift();
+    }
+
+    public isEmpty() : boolean
+    {
+        return this._storage.length > 0;
     }
 }
--- test/structures.tests.ts
+++ test/structures.tests.ts
@@ -1,15 +1,65 @@
 import { expect } from "chai";
 import { Queue } from "../lib/Queue";
 
-describe("Data structures", () => {
-    describe("Queue", () => {
-        it("has size", () => {
+describe("Data structures", () => 
+{
+    describe("Queue", () => 
+    {
+        it("has size", () => 
+        {
             const testee = new Queue();
             const result = testee.size();
             expect(result).to.equal(0);
         })
-    })
 
+        it("increases size", () => 
+        {
+            const testee = new Queue();
+            testee.enqueue("firstValue");
+            testee.enqueue("secondValue");
+
+            const result = testee.size();
+            expect(result).to.equal(2);
+        })
+
+        it("retrieves value", () => 
+        {
+            const testee = new Queue();
+            testee.enqueue("firstValue");
+            testee.enqueue("secondValue");
+
+            const firstResult = testee.poll();
+            const secondResult = testee.poll();
+            expect(secondResult).to.equal("firstValue");
+        })
+
+        it("peeks value", () => 
+        {
+            const testee = new Queue();
+            testee.enqueue("firstValue");
+            testee.enqueue("secondValue");
+
+            const firstResult = testee.peek();
+            const secondResult = testee.peek();
+            expect(secondResult).to.equal(firstResult);
+        })
+
+        it("is empty", () => 
+        {
+            const testee = new Queue();
+
+            expect(testee.isEmpty()).to.equal(true);
+        })
+
+        it("is not empty", () => 
+        {
+            const testee = new Queue();
+            testee.enqueue("firstValue");
+
+            expect(testee.isEmpty()).to.equal(false);
+        })
+
+    })
     // describe("Factory", () => {
     //     it("can create Queue", () => {
     //         const structureType = DataStructures.Queue;
